4c25de967081d7e39d45b11b804b8777e001a8fa
Jimmt <quantum.physics98@gmail.com>
2015-09-20 21:00:28 -0700

    current version

--- core/src/com/jimmt/HologramClock/TimeDisplay.java
+++ core/src/com/jimmt/HologramClock/TimeDisplay.java
@@ -1,11 +1,11 @@
 package com.jimmt.HologramClock;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 
@@ -16,12 +16,16 @@ public class TimeDisplay extends Group {
 
 	public TimeDisplay(BitmapFont font, DisplayEffect effect, float rotation, boolean flipY) {
 		float offset = effect.getOffset();
+		
+		
 		effectActor = new ParticleEffectActor(effect.getPaths()[Prefs.prefs.getInteger("effectIndex")], "effects");
 		effectActor.rotateEffect(rotation);
 		effectActor.setPosition(effectActor.getX() + MathUtils.sinDeg(rotation) * offset,
 				effectActor.getY() + MathUtils.cosDeg(rotation) * offset);
 		LabelStyle style = new LabelStyle();
 		style.font = font;
+		Image bar = new Image(Assets.getTex("bar.png"));
+		style.background = bar.getDrawable();
 		style.fontColor = Color.WHITE;
 
 		label = new Label("00:00:00", style);
@@ -34,9 +38,15 @@ public class TimeDisplay extends Group {
 		offset += effect.getEffectOffset();
 		container.setPosition(container.getX() + MathUtils.sinDeg(rotation) * offset,
 				container.getY() + MathUtils.cosDeg(rotation) * offset);
+	
 
 		addActor(effectActor);
 		addActor(container);
+	}
+	
+	@Override
+	public void act(float delta){
+		super.act(delta);
 	}
 
 }
